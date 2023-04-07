151cf2e02fcd0e7518783618eb361fa6c5092ffa
jsus <esseswann@gmail.com>
2023-04-07 12:43:11 +0300

    getFromEnv fixes

--- src/getFromEnv.ts
+++ src/getFromEnv.ts
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv'
-import invariant from 'invariant'
 dotenv.config()
 
 const getFromEnv = (key: string) => {
   const value = process.env[key]
-  invariant(value, `Provide ${key} environment variable`)
+  if (!value) throw new Error(`Provide ${key} environment variable`)
   return value
 }
 
