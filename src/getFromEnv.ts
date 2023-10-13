420f549a9df13ac32f82dc50d5a84d98ad93c176
jsus <esseswann@yandex.ru>
2023-10-13 12:04:25 +0300

    Removed explicit dotenv setup

--- src/getFromEnv.ts
+++ src/getFromEnv.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 export const getFromEnv = (key: string) => {
   const value = process.env[key]
   if (!value) throw new Error(`Provide ${key} environment variable`)
